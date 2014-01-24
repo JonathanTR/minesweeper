9caac42c5cc9d03942eddf45d6db1e284b7b0dd5
JonathanTR <Jonathan.d.reilly@gmail.com>
2014-01-23 17:22:29 -0700

    Add json version of the board to /new route

--- minesweeper.rb
+++ minesweeper.rb
@@ -3,11 +3,13 @@ require './board'
 require './cell'
 require 'rubygems'
 require 'sinatra'
+require 'json'
 
 get '/' do
   "Welcome to MineSweeper!"
 end
 
 get '/new' do
-
+  @board = Board.new(10, 10, 20)
+  JSON.generate(grid: @board.grid)
 end
